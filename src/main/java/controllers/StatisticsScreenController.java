dac68f508a24bb8e45ab4f42b01ff46222c0293f
Robin <capibaralover@gmail.com>
2022-01-04 15:10:54 +0000

    Choice bpx

--- src/main/java/controllers/StatisticsScreenController.java
+++ src/main/java/controllers/StatisticsScreenController.java
@@ -6,9 +6,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
-import javafx.scene.input.InputMethodEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import models.Strain;
 
@@ -18,23 +17,24 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public  class StatisticsScreenController extends BaseController implements Initializable {
-    public Boolean rainfall= false;
-    public Boolean temp= false;
-    public Boolean wind= false;
+    public String influence = null;
     public int bridgeAge;
 
-
     public LineChart<Float, Float> statGraph;
-
     public CategoryAxis XLabel;
     public Text ageLabel;
     public Slider ageSlider;
+    public ChoiceBox influenceBox;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        influenceBox.getItems().addAll("Rainfall", "Temperature", "Wind");
+        influenceBox.getSelectionModel().select("Rainfall");;
+
         XYChart.Series set = new XYChart.Series();
         LocalDateTime from = LocalDateTime.of(LocalDate.of(2019+getBridgeAge(),1,1), LocalTime.of(0,0,0));
         LocalDateTime to = LocalDateTime.of(LocalDate.of(2020+getBridgeAge(),1,1), LocalTime.of(0,0,0));
@@ -46,36 +46,21 @@ public  class StatisticsScreenController extends BaseController implements Initi
         }
 
 
-
-
-        set1.getData().add(new XYChart.Data("Strain #10201", -461,74));
-        set1.getData().add(new XYChart.Data("Strain #10202", -778.13));
-        set1.getData().add(new XYChart.Data("Strain #10203", -373,17));
-        set1.getData().add(new XYChart.Data("Strain #10204", -1094,65));
-        set1.getData().add(new XYChart.Data("Strain #10205", -822.85));
-        set1.getData().add(new XYChart.Data("Strain #10206", -670.28));
-
-        statGraph.getData().addAll(set1);
+        statGraph.getData().addAll(set);
     }
 
     public void addStrains(List<Strain> strainList, XYChart.Series set){
-        for (Strain strain: list){
-            set.getData().add(new XYChart.Data(, -461,74));
+        if (Objects.equals(getInfluence(), "Rainfall")){
+            for (Strain strain: strainList){
+                set.getData().add(new XYChart.Data(21, strain.getWaarde(),5));
+            }
         }
+
     }
 
-    public void changeXLabel(){
-        StringBuilder text = new StringBuilder();
-        if (getRainfall()){
-            text.append("Rainfall ");
-        }
-        if (getWind()){
-            text.append("Wind ");
-        }
-        if (getTemp()){
-            text.append("Temperature");
-        }
-        XLabel.setLabel(text.toString());
+    public void ChangeInfluence(ActionEvent mouseEvent) {
+        setInfluence((String) influenceBox.getValue());
+        XLabel.setLabel(getInfluence());
     }
 
     public void changeAgeLabel(){
@@ -83,43 +68,13 @@ public  class StatisticsScreenController extends BaseController implements Initi
         ageLabel.setText(String.valueOf(getBridgeAge()));
     }
 
-    public void CheckRainfall(ActionEvent actionEvent) {
-        setRainfall(!getRainfall());
-        changeXLabel();
+
+    public String getInfluence() {
+        return influence;
     }
 
-    public void CheckTemp(ActionEvent actionEvent) {
-        setTemp(!getTemp());
-        changeXLabel();
-    }
-
-    public void CheckWind(ActionEvent actionEvent) {
-        setWind(!getWind());
-        changeXLabel();
-    }
-
-    public Boolean getRainfall() {
-        return rainfall;
-    }
-
-    public void setRainfall(Boolean rainfall) {
-        this.rainfall = rainfall;
-    }
-
-    public Boolean getTemp() {
-        return temp;
-    }
-
-    public void setTemp(Boolean temp) {
-        this.temp = temp;
-    }
-
-    public Boolean getWind() {
-        return wind;
-    }
-
-    public void setWind(Boolean wind) {
-        this.wind = wind;
+    public void setInfluence(String influence) {
+        this.influence = influence;
     }
 
     public int getBridgeAge() {
@@ -129,4 +84,6 @@ public  class StatisticsScreenController extends BaseController implements Initi
     public void setBridgeAge(int bridgeAge) {
         this.bridgeAge = bridgeAge;
     }
+
+
 }
