a0181583ebbf80ea6eced8c0c0a6808574677c47
JuliaVeronique <juliaveronique@gmail.com>
2022-01-11 17:03:45 +0100

    fixed strainFile constructor and TempStrainCompacter.compactCSVFile added comments in them and ReadStrain

--- src/main/java/dataLayer/TempStrainCompacter.java
+++ src/main/java/dataLayer/TempStrainCompacter.java
@@ -13,54 +13,45 @@ import java.util.List;
 public class TempStrainCompacter {
     //do not run again!!!
     public static void compactCSVFile() throws IOException {
-        for (int i = 3; i < 8; i++) {
-            List<Object> groupFile = List.of(new File("SensordataBridgeProjectApplicationDevelopment\\strain-group" + (i + 1)).list());
+        for (int i = 7; i < 8; i++) { //iterates over the 8 groups
+            // looks at the number and name of the files in that group
+            List<String> groupFile = List.of(new File("SensordataBridgeProjectApplicationDevelopment\\strain-group" + (i + 1)).list());
             for (int j = 0; j < groupFile.size(); j++) {
-                int count = j;
-                List<Strain> strains;
-                while (true) {
-                    try {
-                        strains = ReadStrain.getStrains(i + 1, count + 1);
-                        break;
-                    } catch (Exception e) {
-                        count++;
-                    }
-                }
+                List<Strain> strains = ReadStrain.getStrains(i + 1, j + 1); //collects ALL data
                 List<String[]> data = new ArrayList<>();
 
-                LocalDateTime beginDate = strains.get(0).getDateTime();
-                LocalDateTime currentDate = beginDate.withHour(0).withMinute(0).withSecond(0);
+                LocalDateTime beginDate = strains.get(0).getDateTime(); //gets date from first datapoint
+                LocalDateTime currentDate = beginDate.withHour(0).withMinute(0).withSecond(0); //uses start of beginDate
 
                 float counter = 0;
                 float sumStrain = 0;
                 float maxStrain = 0;
                 for (Strain strain : strains) {
-                    if (Float.NaN==strain.getWaarde()){
-                        System.out.println(strain);
-                        continue;
-                    }
-
-                    else if (strain.getDateTime().isBefore(currentDate.plusDays(1))) {
-                        if (Math.abs(strain.getWaarde()) > Math.abs(maxStrain)) {
+                    if (strain.getDateTime().isBefore(currentDate.plusDays(1))) { //checks if the strain is still current date
+                        if (Math.abs(strain.getWaarde()) > Math.abs(maxStrain)) { //updates maximum if it is higher than the current max
                             maxStrain = strain.getWaarde();
                         }
                         sumStrain += strain.getWaarde();
                         counter++;
 
-                    } else {
+                    } else { // if not current date
+                        // fills in a new datapoint of one day with ("date","average","max")
                         data.add(new String[]{String.valueOf(currentDate), String.valueOf(sumStrain / counter), String.valueOf(maxStrain)});
-                        currentDate = currentDate.plusDays(1);
+                        currentDate = currentDate.plusDays(1); //goes to the next day
+                        //begin next day
                         sumStrain = strain.getWaarde();
                         maxStrain = strain.getWaarde();
                         counter = 1;
 
                     }
                 }
+                //create new file in directory
                 File file = new File("SensordataBridgeProjectApplicationDevelopment\\strain-group" + (i + 1) + "Compact\\" + groupFile.get(j));
                 file.createNewFile();
 
-                //        https://mkyong.com/java/how-to-export-data-to-csv-file-java/
+                // https://mkyong.com/java/how-to-export-data-to-csv-file-java/
                 try (CSVWriter writer = new CSVWriter(new FileWriter(file))) {
+                    // write all the data in the csv file
                     writer.writeAll(data);
                 }
             }
