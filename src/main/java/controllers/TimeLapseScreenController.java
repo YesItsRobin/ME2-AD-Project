d66e1a9b9f5b564d3e2c44f5ac3405ab159da89c
Robin <capibaralover@gmail.com>
2022-01-12 20:13:29 +0100

    update chart

--- src/main/java/controllers/TimeLapseScreenController.java
+++ src/main/java/controllers/TimeLapseScreenController.java
@@ -32,21 +32,24 @@ import models.Strain;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 
 public class TimeLapseScreenController extends BaseController implements Initializable{
-    public MenuItem group1;
-    public MenuItem group2;
-    public MenuItem group3;
-    public MenuItem group4;
-    public MenuItem group5;
-    public MenuItem group6;
-    public MenuItem group7;
-    public MenuItem group8;
+    XYChart.Series<Number, Number> series = new XYChart.Series<>();
     public NumberAxis xAxis;
     public NumberAxis yAxis;
+    public CheckBox rainCheck;
+    public CheckBox tempCheck;
+    public CheckBox windCheck;
+    public CheckBox noneCheck;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
     private Stage dialogStage;
-    public int group;
+    public int group = 1;
+    private boolean rain = false;
+    private boolean wind = false;
+    private boolean temp = false;
+    private int maxAge;
+
 
     public void setDialogStage(Stage dialogStage) {
         this.dialogStage = dialogStage;
@@ -63,9 +66,6 @@ public class TimeLapseScreenController extends BaseController implements Initial
     @FXML
     private void activate(ActionEvent event) {
     }
-
-
-
     @FXML
     private Button homeButton;
 
@@ -87,7 +87,7 @@ public class TimeLapseScreenController extends BaseController implements Initial
         timelapseChart.setTitle("Chart");
 
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
-        series.setName("Strain - Group 1");
+        series.setName(getName());
 
         try {
             ArrayList<CompactStrain> strains = ReadCompactStrain.getStrains(1, 1);
@@ -95,11 +95,57 @@ public class TimeLapseScreenController extends BaseController implements Initial
             for (int i = 0; i < maxAge; i++) {
                 series.getData().add(new XYChart.Data<>(i, reg.getY(i)));
             }
-        } catch (IOException ignored) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
         timelapseChart.getData().add(series);
 
+    }
+
+    public void updateSeries(){
+        int maxAge=600;
+        xAxis.setAutoRanging(false);
+        xAxis.setLowerBound(0);
+        xAxis.setUpperBound(maxAge);
+
+        yAxis.setAutoRanging(false);
+        yAxis.setLowerBound(-300);
+        yAxis.setUpperBound(100);
+        timelapseChart.setTitle("Chart");
+
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName(getName());
+
+        try {
+            ArrayList<CompactStrain> strains = ReadCompactStrain.getStrains(1, 1);
+            SimRegression reg = new SimRegression(strains, Influences.age);
+            for (int i = 0; i < maxAge; i++) {
+                series.getData().add(new XYChart.Data<>(i, reg.getY(i)));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        timelapseChart.getData().add(series);
+    }
+
+    private String getName(){
+        StringBuilder str = new StringBuilder();
+        if (isWind()){
+            str.append("wind ");
+        }
+        if (isRain()){
+            str.append("rain ");
+        }
+        if (isTemp()){
+            str.append("temperature ");
+        }
+        if (getGroup()==999) {
+            str.append("All groups.");
+        }
+        else{
+            str.append(getGroup());
+        }
+        return str.toString();
     }
 
     public int getGroup() {
@@ -111,34 +157,101 @@ public class TimeLapseScreenController extends BaseController implements Initial
     }
 
     public void group1(ActionEvent actionEvent) {
+        strainGroupMenu.setText("Straingroup 1");
         setGroup(1);
     }
 
     public void group2(ActionEvent actionEvent) {
+        strainGroupMenu.setText("Straingroup 2");
         setGroup(2);
     }
 
     public void group3(ActionEvent actionEvent) {
+        strainGroupMenu.setText("Straingroup 3");
         setGroup(3);
     }
     public void group4(ActionEvent actionEvent) {
+        strainGroupMenu.setText("Straingroup 4");
         setGroup(4);
     }
 
     public void group5(ActionEvent actionEvent) {
+        strainGroupMenu.setText("Straingroup 5");
         setGroup(5);
     }
 
     public void group6(ActionEvent actionEvent) {
+        strainGroupMenu.setText("Straingroup 6");
         setGroup(6);
     }
 
     public void group7(ActionEvent actionEvent) {
+        strainGroupMenu.setText("Straingroup 7");
         setGroup(7);
     }
 
     public void group8(ActionEvent actionEvent) {
+        strainGroupMenu.setText("Straingroup 8");
         setGroup(8);
+    }
+
+    public void allGroups(ActionEvent actionEvent) {
+        strainGroupMenu.setText("All groups");
+        setGroup(999);
+    }
+
+    public void switchRain(ActionEvent actionEvent) {
+        setRain(!isRain());
+        noneCheck.setSelected(false);
+    }
+
+    public void switchWind(ActionEvent actionEvent) {
+        setWind(!isWind());
+        noneCheck.setSelected(false);
+    }
+
+    public void switchTemp(ActionEvent actionEvent) {
+        setWind(!isWind());
+        noneCheck.setSelected(false);
+    }
+
+    public void switchNone(ActionEvent actionEvent) {
+        setWind(false);
+        setRain(false);
+        setTemp(false);
+
+        windCheck.setSelected(false);
+        rainCheck.setSelected(false);
+        tempCheck.setSelected(false);
+    }
+
+    public boolean isRain() {
+        return rain;
+    }
+
+    public void setRain(boolean rain) {
+        this.rain = rain;
+    }
+
+    public boolean isWind() {
+        return wind;
+    }
+
+    public void setWind(boolean wind) {
+        this.wind = wind;
+    }
+
+    public boolean isTemp() {
+        return temp;
+    }
+
+    public void setTemp(boolean temp) {
+        this.temp = temp;
+    }
+
+    public void Redraw(ActionEvent actionEvent) {
+        timelapseChart.getData().remove(0);
+        updateSeries();
     }
 
 }
--- src/main/java/models/CompactStrain.java
+++ src/main/java/models/CompactStrain.java
@@ -7,10 +7,10 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 public class CompactStrain {
+    private final CompactMeteo meteo;
     private LocalDate date;
     private float average;
     private float max;
-    private static List<CompactMeteo> meteos = ReadCompactMeteo.getMeteo();
     private final static LocalDate beginDate = LocalDate.of(2019, 11, 27);
 
 
@@ -18,10 +18,11 @@ public class CompactStrain {
         this.date = date;
         this.average = average;
         this.max = max;
+        this.meteo = findMeteo();
     }
 
-    public CompactMeteo getMeteo() {
-        for (CompactMeteo meteo : this.meteos) {
+    private CompactMeteo findMeteo() {
+        for (CompactMeteo meteo : ReadCompactMeteo.getMeteo()) {
             if (meteo.getDate().isEqual(this.date)) return meteo;
         }
         return null;
