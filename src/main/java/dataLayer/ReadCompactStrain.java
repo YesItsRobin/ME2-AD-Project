477c5c5cde1b7f9362c1ea4ba1d042522c96d304
JuliaVeronique <juliaveronique@gmail.com>
2022-01-14 14:18:55 +0100

    Created TempStrainGroup to compact the data of each group in one file by day and created ReadCompactStrain.getStrainsGroup that returns a list of all strains of a group

--- src/main/java/dataLayer/ReadCompactStrain.java
+++ src/main/java/dataLayer/ReadCompactStrain.java
@@ -1,27 +1,35 @@
 package dataLayer;
 
 import models.CompactStrain;
-import models.Strain;
 
+import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReadCompactStrain extends BaseReader {
     //getStrains returns a pretty Arraylist of Strains of a given group and number
-
-
     public static ArrayList<CompactStrain> getStrains(int group, int number) throws IOException {
-            StrainFile file = new StrainFile(group, number, true);        //Uses the StrainFile class to get access to that group and number's csv
-            ArrayList<String> data = readCSV(file.getPath());       //Calls the BaseReader, gets an ugly ArrayList back
-            ArrayList<CompactStrain> strains = new ArrayList<>();    //Creates an empty arraylist of Strains
-            for (String row : data) {
-                strains.add(buildStrain(row));
-                }
-            return strains;
+        StrainFile file = new StrainFile(group, number, true);        //Uses the StrainFile class to get access to that group and number's csv
+        ArrayList<String> data = readCSV(file.getPath());       //Calls the BaseReader, gets an ugly ArrayList back
+        ArrayList<CompactStrain> strains = new ArrayList<>();    //Creates an empty arraylist of Strains
+        for (String row : data) {
+            strains.add(buildStrain(row));
         }
+        return strains;
+    }
+
+    public static ArrayList<CompactStrain> getStrainsGroup(int group) throws IOException {
+        int numOfGroups = List.of(new File("SensordataBridgeProjectApplicationDevelopment\\strain-group" + group + "Compact").list()).size();
+        ArrayList<CompactStrain> strainsGroup = new ArrayList<>();
+        for (int i = 0; i < numOfGroups; i++) {
+            for (CompactStrain strain : getStrains(group, i + 1)) {
+                strainsGroup.add(strain);
+            }
+        }
+        return strainsGroup;
+    }
 
     //Turns an ugly String row into a usable Strain
     private static CompactStrain buildStrain(String row) {
